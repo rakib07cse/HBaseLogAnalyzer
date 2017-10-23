d859cc0b9d67f84c68282bb8ce36d8213dbd707a
Rakibul hassan <rakib07cse@gmail.com>
2017-10-23 11:07:05 +0600

    Added table depends on log type

--- src/main/java/com/ipvision/analyzer/hbase/HBaseWriter.java
+++ src/main/java/com/ipvision/analyzer/hbase/HBaseWriter.java
@@ -21,16 +21,16 @@ import org.slf4j.LoggerFactory;
  * @author rakib
  */
 public class HBaseWriter {
-
+    
     private static final Logger hBaseInserterLogger = LoggerFactory.getLogger(HBaseWriter.class);
     private static final SimpleDateFormat tableNameFormat = new SimpleDateFormat("yyyyMM");
-
+    
     public static void insertIntoHBase(List<LogBean> logBeanList) throws IOException {
         long timeStamp = System.currentTimeMillis();
         String methodTable = tableNameFormat.format(timeStamp) + "_method";
         String liveStreamTable = tableNameFormat.format(timeStamp) + "_liveStream";
         String errorTable = tableNameFormat.format(timeStamp) + "_error";
-
+        
         if (!HBaseManager.getHBaseManager().getAdmin().tableExists(methodTable)) {
             HBaseManager.getHBaseManager().createHBaseTable(methodTable);
         }
@@ -40,54 +40,57 @@ public class HBaseWriter {
         if (!HBaseManager.getHBaseManager().getAdmin().tableExists(errorTable)) {
             HBaseManager.getHBaseManager().createHBaseTable(errorTable);
         }
-
+        
         try {
             HTable methodHTable = HBaseManager.getHBaseManager().createHTable(methodTable);
             HTable liveStreamHTable = HBaseManager.getHBaseManager().createHTable(liveStreamTable);
             HTable errorHTable = HBaseManager.getHBaseManager().createHTable(errorTable);
-
+            
             List<Put> methodBeanPutList = new ArrayList<>();
             List<Put> liveStreamBeanPutList = new ArrayList<>();
             List<Put> errorBeanPutList = new ArrayList<>();
             
-            
             for (LogBean logBean : logBeanList) {
-
+                
                 if (logBean.getTimestamp() != null) {
-                    Put logMethodPut = HBaseManager.getHBaseManager().createLoggerPut(logBean.getTimestamp());
-                    Put logLiveStreamPut = HBaseManager.getHBaseManager().createLoggerPut(logBean.getTimestamp());
-                    Put logErrorPut = HBaseManager.getHBaseManager().createLoggerPut(logBean.getTimestamp());
-
-                    if (logBean.getMethodName() != null) {
-                        logMethodPut.add(Bytes.toBytes(Tools.HBASE_TABLE_METHOD_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_METHOD_FIRST_COLUME_NAME), Bytes.toBytes(logBean.getMethodName()));
-                    }
-                    if (logBean.getParams() != null) {
-                        logMethodPut.add(Bytes.toBytes(Tools.HBASE_TABLE_METHOD_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_METHOD_SECOND_COLUME_NAME), Bytes.toBytes(logBean.getParams()));
-                    }
                     if (logBean.getLiveStreamHistory() != null) {
-                        logLiveStreamPut.add(Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_FIRST_COLUME_NAME), Bytes.toBytes(logBean.getLiveStreamHistory()));
+                        
+                        Put liveStreamPut = HBaseManager.getHBaseManager().createLoggerPut(logBean.getTimestamp());
+                        if (logBean.getLiveStreamHistory() != null) {
+                            liveStreamPut.add(Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_FIRST_COLUME_NAME), Bytes.toBytes(logBean.getLiveStreamHistory()));
+                        }
+                        if (logBean.getLiveStreamParams() != null) {
+                            liveStreamPut.add(Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_SECOND_COLUME_NAME), Bytes.toBytes(logBean.getLiveStreamParams()));
+                        }
+                        liveStreamBeanPutList.add(liveStreamPut);
+                        
+                    } else if ("ERROR".equals(logBean.getLogLevel()) || "WARN".equals(logBean.getLogLevel()) || "FATAL".equals(logBean.getLogLevel())) {
+                        Put errorPut = HBaseManager.getHBaseManager().createLoggerPut(logBean.getTimestamp());
+                        if (logBean.getLogLevel() != null) {
+                            errorPut.add(Bytes.toBytes(Tools.HBASE_TABLE_NOTINFO_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_NOTINOF_FIRST_COLUME_NAME), Bytes.toBytes(logBean.getLogLevel()));
+                        }
+                        if (logBean.getEventType() != null) {
+                            errorPut.add(Bytes.toBytes(Tools.HBASE_TABLE_NOTINFO_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_NOTINFO_SECOND_COLUME_NAME), Bytes.toBytes(logBean.getEventType()));
+                        }
+                        errorBeanPutList.add(errorPut);
+                    } else {
+                        Put methodPut = HBaseManager.getHBaseManager().createLoggerPut(logBean.getTimestamp());
+                        if (logBean.getMethodName() != null) {
+                            methodPut.add(Bytes.toBytes(Tools.HBASE_TABLE_METHOD_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_METHOD_FIRST_COLUME_NAME), Bytes.toBytes(logBean.getMethodName()));
+                        }
+                        if (logBean.getParams() != null) {
+                            methodPut.add(Bytes.toBytes(Tools.HBASE_TABLE_METHOD_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_METHOD_SECOND_COLUME_NAME), Bytes.toBytes(logBean.getParams()));
+                        }
+                        methodBeanPutList.add(methodPut);
                     }
-                    if (logBean.getLiveStreamParams() != null) {
-                        logLiveStreamPut.add(Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_LIVESTREAMHISTORY_SECOND_COLUME_NAME), Bytes.toBytes(logBean.getLiveStreamParams()));
-                    }
-                    if (logBean.getLogLevel() != null) {
-                        logErrorPut.add(Bytes.toBytes(Tools.HBASE_TABLE_NOTINFO_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_NOTINOF_FIRST_COLUME_NAME), Bytes.toBytes(logBean.getLogLevel()));
-                    }
-                    if (logBean.getEventType() != null) {
-                        logErrorPut.add(Bytes.toBytes(Tools.HBASE_TABLE_NOTINFO_COLUME_FAMILY_NAME), Bytes.toBytes(Tools.HBASE_TABLE_NOTINFO_SECOND_COLUME_NAME), Bytes.toBytes(logBean.getEventType()));
-                    }
-
-                    methodBeanPutList.add(logMethodPut);
-                    liveStreamBeanPutList.add(logLiveStreamPut);
-                    errorBeanPutList.add(logErrorPut);
-
+                    
                 }
             }
-
+            
             methodHTable.put(methodBeanPutList);
             liveStreamHTable.put(liveStreamBeanPutList);
             errorHTable.put(errorBeanPutList);
-
+            
             if (methodHTable != null) {
                 methodHTable.flushCommits();
             }
@@ -95,16 +98,16 @@ public class HBaseWriter {
                 liveStreamHTable.flushCommits();
             }
             if (errorHTable != null) {
-                System.out.println("Insert error HTable");
                 errorHTable.flushCommits();
             }
             String msg = "Data insert successfully";
             hBaseInserterLogger.info(msg);
         } catch (Exception ex) {
             String msg = "";
+            ex.printStackTrace();
             hBaseInserterLogger.error(msg);
         }
-
+        
     }
-
+    
 }
